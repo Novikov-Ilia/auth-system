8684a195936714e341622dbb5ce37321119e6ed0
Ilya <novikov.ilia@phystech.edu>
2026-07-16 21:34:00 +0300

    feat(users): Add custom user model and manager, update README

---
+++ users/managers.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    @staticmethod
+    def normalize_email(email: str) -> str:
+        email = email.strip()
+
+        if not email:
+            raise ValueError('Передана пустая почта')
+        
+        name, domain = email.rsplit('@', 1)
+        norm_email = name + '@' + domain.lower()
+
+        return norm_email
+
+    def create_user(self, /, email: str, passwd: str, **extra_fields: dict):
+        if not email:
+            raise ValueError("Email обязателен")
+
+        if not passwd:
+            raise ValueError("Пароль обязателен")
+
+        norm_email = UserManager.normalize_email(email)
+        
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', False)
+
+        user = self.model(
+            email=norm_email,
+            **extra_fields,
+        )
+        user.set_password(passwd)
+        user.save()
+
+        return user
+
+    def create_superuser(self, /, email: str, passwd: str, **extra_fields: dict):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(
+                "У суперпользователя is_staff должен быть True"
+            )
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(
+                "У суперпользователя is_superuser должен быть True"
+            )
+
+        return self.create_user(email=email, passwd=passwd, **extra_fields)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at= models.DateTimeField(null=True, blank=True)
+
+    role = models.ForeignKey(
+        'access_control.Role',
+        related_name='users',
+        on_delete=models.RESTRICT,
+    )
+
+    USERNAME_FIELD = 'email'
+    objects = UserManager()
