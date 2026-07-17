694b22959e7262ddfe09f1999f23b910bec9d5b8
Ilya <novikov.ilia@phystech.edu>
2026-07-17 15:22:38 +0300

    feat(users): Add registration APIView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegistrationSerializer
 
-# Create your views here.
+class RegistrationView(APIView):
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        user = serializer.save()
+
+        return Response(
+            {
+                'id': user.id,
+                'email': user.email,
+            },
+            status=status.HTTP_201_CREATED,
+        )
