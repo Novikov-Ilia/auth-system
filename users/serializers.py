66f8fa8ac6b5c957b59c275401c5df9cb648c772
Ilya <novikov.ilia@phystech.edu>
2026-07-17 13:10:00 +0300

    feat(users): Add RegistrationSerializer

---
+++ users/serializers.py
@@ -0,0 +1,67 @@
+from django.core.validators import EmailValidator
+from django.contrib.auth.password_validation import validate_password as django_validate_password
+from django.core.exceptions import ValidationError as DjangoValidationError
+from rest_framework.validators import UniqueValidator
+from rest_framework import serializers
+from .models import User
+from access_control.models import Role
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    repeat_password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                lookup='iexact',
+                message='Пользователь с таким email уже существует.',
+            ),
+            EmailValidator(),
+        ],
+    )
+    class Meta:
+        model = User
+        fields = [
+            'repeat_password',
+            'password',
+            'email',
+            'first_name',
+            'last_name', 
+            'middle_name',
+        ]
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+            },
+        }
+
+
+    def validate_password(self, password: str, user=None) -> str:
+        try:
+            django_validate_password(password, user=user)
+        except DjangoValidationError as error:
+            raise serializers.ValidationError(error.messages) from error
+
+        return password
+
+    def validate(self, attrs: dict) -> dict:
+        password = attrs.get('password')
+        repeat_password = attrs.get('repeat_password')
+
+        if not password == repeat_password:
+            raise DjangoValidationError('Пароли должны совпадать')
+        
+        return attrs
+    
+    def create(self, validated_data: dict)  -> User:
+        validated_data.pop('repeat_password')
+        password = validated_data.pop("password")
+        email = validated_data.pop("email")
+        role = Role.objects.get(code="user")
+
+        return User.objects.create_user(
+            email=email,
+            passwd=password,
+            role=role,
+            **validated_data,
+        )
