035ba99fe01c2528ef07ddc7e8aa65887a35fb67
Ilya <novikov.ilia@phystech.edu>
2026-07-20 16:51:00 +0300

    feat(users): Add Login APIView

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import RegistrationSerializer
+from .serializers import LoginSerializer, RegistrationSerializer
+from services import jwt
 
 class RegistrationView(APIView):
     def post(self, request):
@@ -17,3 +18,16 @@ class RegistrationView(APIView):
             },
             status=status.HTTP_201_CREATED,
         )
+    
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        token = jwt.issue_access_token(serializer.validated_data['user'])
+
+        return Response({
+            'access_token': token,
+            "token_type": "Bearer",
+            "expires_in": 3600
+        })
