7e6521ac8b8359aa04e168811caa48a73c4f5d09
Ilya <novikov.ilia@phystech.edu>
2026-07-21 16:18:19 +0300

    feat(access_control): Add BusinessElement model

--- access_control/models.py
+++ access_control/models.py
@@ -11,3 +11,10 @@ class Role(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class BusinessElement(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
