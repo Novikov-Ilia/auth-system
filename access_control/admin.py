22b546e335126a181bd24e9078dc5ab36dfd9b45
Ilya <novikov.ilia@phystech.edu>
2026-07-21 16:24:35 +0300

    feat(admin): register application models

--- access_control/admin.py
+++ access_control/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Role
+
+from .models import BusinessElement, Role
 
 
-admin.site.register(Role)
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "created_at")
+    search_fields = ("code", "name")
+    readonly_fields = ("created_at",)
+
+
+@admin.register(BusinessElement)
+class BusinessElementAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "created_at")
+    search_fields = ("code", "name", "description")
+    readonly_fields = ("created_at",)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import AuthSession, User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    ordering = ("email",)
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "role",
+        "is_active",
+        "is_staff",
+        "deleted_at",
+    )
+    list_filter = ("is_active", "is_staff", "role")
+    search_fields = ("email", "first_name", "last_name")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "middle_name", "role")} ),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")} ),
+        ("Important dates", {"fields": ("last_login", "created_at", "updated_at", "deleted_at")} ),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "first_name", "last_name", "middle_name", "role", "password1", "password2"),
+        }),
+    )
+    readonly_fields = ("created_at", "updated_at", "deleted_at")
+
+
+@admin.register(AuthSession)
+class AuthSessionAdmin(admin.ModelAdmin):
+    list_display = ("user", "jti", "created_at", "expires_at", "revoked_at")
+    list_filter = ("revoked_at",)
+    search_fields = ("user__email", "jti")
+    readonly_fields = ("id", "jti", "created_at")
