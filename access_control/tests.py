f84cac878e37298a22764f5fc96a04e0f504d74f
Ilya <novikov.ilia@phystech.edu>
2026-07-21 16:39:49 +0300

    test(access_control): Add tests for new models

--- access_control/tests.py
+++ access_control/tests.py
@@ -1,7 +1,7 @@
 from django.db import IntegrityError, transaction
 from django.test import TestCase
 
-from access_control.models import Role
+from access_control.models import AccessRoleRule, BusinessElement, Role
 
 
 class RoleModelTests(TestCase):
@@ -37,3 +37,35 @@ class RoleModelTests(TestCase):
 
     def test_model_uses_roles_table(self):
         self.assertEqual(Role._meta.db_table, 'roles')
+
+
+
+class AccessRoleRuleModelTests(TestCase):
+    def setUp(self):
+        self.role = Role.objects.create(code="manager", name="Менеджер")
+        self.element = BusinessElement.objects.create(
+            code="products",
+            name="Products",
+            description="Demo products",
+        )
+
+    def test_rule_defaults_to_no_permissions(self):
+        rule = AccessRoleRule.objects.create(
+            role=self.role,
+            element=self.element,
+        )
+
+        self.assertFalse(rule.read_permission)
+        self.assertFalse(rule.read_all_permission)
+        self.assertFalse(rule.create_permission)
+        self.assertFalse(rule.update_permission)
+        self.assertFalse(rule.update_all_permission)
+        self.assertFalse(rule.delete_permission)
+        self.assertFalse(rule.delete_all_permission)
+        self.assertEqual(str(rule), "manager: products")
+
+    def test_rule_is_unique_for_role_and_element(self):
+        AccessRoleRule.objects.create(role=self.role, element=self.element)
+
+        with self.assertRaises(IntegrityError), transaction.atomic():
+            AccessRoleRule.objects.create(role=self.role, element=self.element)
